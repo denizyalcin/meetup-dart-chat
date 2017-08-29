594ddff46bb3f50ca56c8ba8531ea575f1741b64
Stéphane Este-Gracias <sestegra@gmail.com>
2017-08-29 19:15:50 +0200

    Fix compilation

--- dart_chat_common/lib/shared.dart
+++ dart_chat_common/lib/shared.dart
@@ -1,7 +1,6 @@
 library dart_chat_common.sharing;
 
 export 'src/shared/auth_model.dart';
-export 'src/shared/config.dart';
 export 'src/shared/data_model.dart';
 export 'src/shared/logging.dart';
 export 'src/shared/websocket_manager.dart';
